498292641a22dc29c2e61170dd3fffd45638e87d
johnsqliu <johnsqliu@tencent.com>
2020-10-14 19:00:08 +0800

    添加chapter接口

--- handler/crawler.js
+++ handler/crawler.js
@@ -1,10 +1,24 @@
 const fetch = require('./fetch');
+const iconv = require('iconv-lite');
 
 async function crawler(url, options = {}) {
   try {
     const res = await fetch(url, options);
-    console.log(res.data);
-    return res;
+
+    let charset = 'utf8';
+
+    let html = iconv.decode(res.data, charset);
+
+    const regCharset = /<meta[^>]+charset=['"]?([a-z0-9-]+)['"]?/i;
+    const charsetMatch = html.match(regCharset);
+
+    charset = charsetMatch[1].toLowerCase() === 'utf-8' ? 'utf8' : charsetMatch[1].toLowerCase();
+
+    if (charset !=='utf8') {
+      html = iconv.decode(res.data,charset);
+    }
+
+    return { html, status: res.status };
   } catch (e) {
     throw new Error(`crawler: ${e.message}`);
   }
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -14,25 +14,21 @@ router.post('/crawler', async (req, res) => {
   try {
     const result = await crawler(url, options);
 
-    let charset = 'utf8';
+    return res.json(result);
+  } catch (e) {
+    throw new Error(`crawler api: ${e.message}`)
+  }
+})
 
-    let html = iconv.decode(result.data, charset);
+router.post('/crawler/chapter', async (req, res) => {
+  const { url,options } = req.body;
+  const { bookId } = options;
+  try {
+    const result = await crawler(url, options);
 
-    const regCharset = /<meta[^>]+charset=['"]?([a-z0-9-]+)['"]?/i;
-    const charsetMatch = html.match(regCharset);
+    console.log(bookId);
 
-    charset = charsetMatch[1].toLowerCase() === 'utf-8' ? 'utf8' : charsetMatch[1].toLowerCase();
-
-    if (charset !=='utf8') {
-      html = iconv.decode(result.data,charset);
-    }
-
-    const data = {
-      html,
-      status: result.status,
-    }
-
-    return res.json(data);
+    return res.json(result);
   } catch (e) {
     throw new Error(`crawler api: ${e.message}`)
   }
